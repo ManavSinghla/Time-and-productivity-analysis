ca1e019b8a6a83294e275a5a561be60506909223
Manav Singla <manavsinghla123@gmail.com>
2025-12-14 14:07:40 +0530

    Enable CORS for frontend-backend communication

--- server/server.js
+++ server/server.js
@@ -2,10 +2,13 @@ import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 import taskRoutes from "./routes/taskRoutes.js";
+import cors from "cors";
+
 
 dotenv.config();
-
 const app = express();
+
+app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
@@ -20,3 +23,4 @@ app.use("/api/tasks", taskRoutes);
 app.listen(5000, () => {
     console.log("Server running on http://localhost:5000");
 });
+
