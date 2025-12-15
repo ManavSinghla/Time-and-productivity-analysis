705f0c328905807d35b494ab966706f7bcf3196c
Manav Singla <manavsinghla123@gmail.com>
2025-12-15 12:29:28 +0530

    Integrate analytics dashboard into main app

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,15 @@
 import './App.css'
 import TaskList from "./components/taskList";
-
+import AnalyticsDashboard from "./components/AnalyticsDashboard";
 
 function App() {
   return (
-    <div>
+    <div style={{ padding: "20px" }}>
       <h1>Time and Productivity Analysis</h1>
+      <AnalyticsDashboard />
       <TaskList />
     </div>
   );
 }
 
-export default App
+export default App;
