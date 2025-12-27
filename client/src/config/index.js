ab3b83b22165ec1b9a702284765d1e5c192d4ca6
Manav Singla <manavsinghla123@gmail.com>
2025-12-27 09:37:19 +0530

    fix: Update API URLs and MONGO_URI for local development configuration

--- client/src/config/index.js
+++ client/src/config/index.js
@@ -1,2 +1,2 @@
-export const BASE_API_URL="https://time-and-productivity-analysis-vunu.vercel.app/"
-export const BASE_API_URL_LOCAL="http://localhost:5000/"
+export const BASE_API_URL="https://time-and-productivity-analysis-vunu.vercel.app"
+export const BASE_API_URL_LOCAL="http://localhost:5000"
